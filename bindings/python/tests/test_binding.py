b6e5870e059a09b1258712ad226a47be78f08d56
Vladimir Levin <opaozhub@gmail.com>
2026-01-02 12:29:39 +0900

    chore: Update bindigns

--- bindings/python/tests/test_binding.py
+++ bindings/python/tests/test_binding.py
@@ -1,11 +1,12 @@
 from unittest import TestCase
 
-import tree_sitter, tree_sitter_caddy
+from tree_sitter import Language, Parser
+import tree_sitter_caddy
 
 
 class TestLanguage(TestCase):
     def test_can_load_grammar(self):
         try:
-            tree_sitter.Language(tree_sitter_caddy.language())
+            Parser(Language(tree_sitter_caddy.language()))
         except Exception:
             self.fail("Error loading Caddy grammar")
